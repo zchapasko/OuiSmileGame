57fcebcc6ff36a32b00a69bcc6bebd715fd0ef6a
unknown <elliots@bu.edu>
2015-07-15 16:28:25 +0200

    piano notes mary

--- javascript/game.js
+++ javascript/game.js
@@ -79,7 +79,7 @@ var i = null;
 var timer = 0;
 var score = 0;
 watchTime = true;
-music = [pop, pop];
+music = [B,A,G,A,B,B,B,A,A,A,B,D,D,B,A,G,A,B,B,B,A,A,B,A,G];
 
 //function to repeat 30 times per second, updating the game
 setInterval(function(){
@@ -105,7 +105,7 @@ setInterval(function(){
 			&& (currentBubble.x + bubbleSize) > canvasX  
 			&& currentBubble.y < canvasY 
 			&& (currentBubble.y + bubbleSize) > canvasY){
-				music[bubbleNum %1].play();
+				music[bubbleNum].play();
 				currentBubble.popped = true;
 		}
 		//if bubble has been popped, make a new one
